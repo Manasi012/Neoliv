d96ae456b89a6924a09ca32b3b95085be5b7a513
Manasi012 <mansi@propstory.com>
2024-05-27 10:58:40 +0530

    partner section and pillars section changes done

--- src/Components/Footer/FooterLinks.jsx
+++ src/Components/Footer/FooterLinks.jsx
@@ -6,7 +6,7 @@ const FooterLinks = () => {
   return (
     <div className='bg_indigo w-screen md:h-14 h-20 md:flex block md:justify-between justify-center'>
       <div className='text-white flex justify-center md:ml-20 pt-4 text-xs'>
-        <span className='text-white'>©️ 2024, NeoLiv</span>
+        <p className='text-white'><span>©️ 2024, NeoLiv</span></p>
         <span className='mx-3'>|</span>
         {/* <span className='underline'>SEBI Approval</span> */}
         <a href={MyPDF} target="_blank" rel="noopener noreferrer">SEBI Approval</a>
