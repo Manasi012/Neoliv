ee1e8584f9d937b7d3578fdcea0a912786855813
Manasi012 <mansi@propstory.com>
2024-05-29 12:26:10 +0530

    disclaimer added

--- src/Components/ModalDisclaimer.jsx
+++ src/Components/ModalDisclaimer.jsx
@@ -65,7 +65,8 @@ const ModalDisclaimer = () => {
                   </div>
                 </div>
               </div>
-              <div className="px-4 py-3 flex justify-evenly">
+              
+              <div className="px-4 py-3 md:flex justify-evenly">
                 <NavLink to="/disclaimer" target="_blank">
                   <button
                     className="mt-3 w-full inline-flex justify-center rounded-md underline shadow-sm px-4 py-2 text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
