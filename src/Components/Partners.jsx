11ceab03b68f2a5cc79f1282396afdde4083ea8f
Manasi012 <mansi@propstory.com>
2024-06-03 11:34:36 +0530

    new partners section added

--- src/Components/Partners.jsx
+++ src/Components/Partners.jsx
@@ -153,11 +153,11 @@ const Partners = () => {
         onClick={() => handleImageClick(index)}
         style={{ transition: "box-shadow 0.3s" }}
       >
-        <img src={image.src} alt={image.name} className="w-36 h-auto cursor-pointer border-4 border-indigo-900 rounded-full" />
+        <img src={image.src} alt={image.name} className="w-36 h-auto cursor-pointer border-4 border-indigo-900 rounded-full mb-2" />
         <h3 className="text-center mt-2 font-bold text-[12px]">{image.name}</h3>
        <div className="flex justify-between items-center gap-2 pt-2 cursor-pointer">
-       <p className="text-center text-[14px]">{image.Description}</p>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+       <p className="text-center text-blue-900 text-[14px]">{image.Description}</p>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4 text-blue-900">
   <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
 </svg>
        </div>
