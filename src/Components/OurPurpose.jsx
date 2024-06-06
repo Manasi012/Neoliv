986208a4d788e0341f5bc9ca9b76c3775a1fc4cf
Manasi012 <mansi@propstory.com>
2024-06-06 11:31:36 +0530

    tablet issues fixed

--- src/Components/OurPurpose.jsx
+++ src/Components/OurPurpose.jsx
@@ -18,8 +18,8 @@ const OurPurpose = () => {
       <div className="md:w-1/2 md:h-[500px] h-[440px] relative md:p-20 px-8 py-0 bg_indigo md:-ml-4 md:rounded-none rounded-xl md:-m-0 -mt-5 text-white" data-aos="fade-right" data-aos-delay="800" data-aos-duration="1000">
         <img src={FinalLogo} alt="FinalLogo" className='absolute md:-left-[6.5rem] mobile-center md:top-80 opacity-30 md:w-52 w-40'/>
 
-        <h2 className="text-4xl md:mt-0 mt-32 font-normal absolute md:-left-[5rem] md:top-14 opacity-30 md:w-52 w-40" data-aos="fade-right" data-aos-delay="800" data-aos-duration="1000">Our&nbsp;&nbsp;Purpose</h2>
-        <p className='md:pt-40 pt-20 pb-4 md:me-56 lg:me-0 text-xl leading-8 absolute md:top-7' data-aos="fade-right" data-aos-delay="800" data-aos-duration="1000">To re-ignite the joy of home owning and living experience, by being the most customer-centric developer in India</p>
+        <h2 className="text-4xl md:mt-0 mt-32 font-normal absolute md:-left-[5rem] left-16 md:top-14 opacity-30 md:w-52 w-40" data-aos="fade-right" data-aos-delay="800" data-aos-duration="1000">Our&nbsp;&nbsp;Purpose</h2>
+        <p className='md:pt-40 pt-20 pb-4 md:me-56 lg:me-0 text-xl leading-8 absolute md:top-7 top-36' data-aos="fade-right" data-aos-delay="800" data-aos-duration="1000">To re-ignite the joy of home owning and living experience, by being the most customer-centric developer in India</p>
       </div>
     </div>
   )
