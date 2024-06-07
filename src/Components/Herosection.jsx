1d032bafaa4795d88a027ada9c5f363999e332d5
Manasi012 <mansi@propstory.com>
2024-06-07 12:33:07 +0530

    new changes done as per client requirements

--- src/Components/Herosection.jsx
+++ src/Components/Herosection.jsx
@@ -71,28 +71,28 @@ const Herosection = () => {
 
         <div className="hidden md:block">
           <img
-            src="https://cdn.propstory.com/magicpages/NAME/16iwl4kl6ilx01kmq6neewdesk.jpg"
+            src="https://cdn.propstory.com/magicpages/NAME/16iwl4kl6ilx4c8otadesktop_neoliv_final.jpg"
             alt="bannerimage"
             className="w-full h-screen sm_screen banner_tab"
           />
         </div>
         <div className="md:hidden">
           <img
-            src="https://cdn.propstory.com/magicpages/NAME/16iwl4kl6ilx097mmfnewupdeted.jpg"
+            src="https://cdn.propstory.com/magicpages/NAME/16iwl4kl6ilx4c95clmobile_neoliv_final.jpg"
             alt="bannerimage"
             className="w-full h-screen"
           />
         </div>
-        <div className="absolute inset-0 bg-black opacity-50"></div>
+        {/* <div className="absolute inset-0 bg-black opacity-50"></div> */}
 
         <div className="absolute inset-0 flex flex-col items-center justify-center text-center md:mx-10">
-          <h1
+          {/* <h1
             className="text-white uppercase md:text-xl md:-mt-[29rem]  -mt-[29rem] md:leading-[50px] leading-normal font-medium text-sm px-2 tablet_headline"
             style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)" }}
           >
             India’s foremost integrated fund led residential real estate
             developer
-          </h1>
+          </h1> */}
           <div className="absolute md:-bottom-8 bottom-2">
             <p
               className="text-center md:px-0 px-3 tablet_para py-20 text-white md:leading-7 leading-5 font-medium md:text-[16px] text-sm"
